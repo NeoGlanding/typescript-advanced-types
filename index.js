35d29da961bce92cd7f6e8333a21a13a4dd69e0f
NeoGlanding <neoglanding@protonmail.com>
2021-11-27 18:33:32 +0700

    intersection with interfaces and types

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 "use strict";
+var ihsan = {
+    name: "Ihsan",
+    education: 'Vocational High School',
+    contractYear: 1
+};
+var hafizh = {
+    name: 'Hafizh',
+    education: 'Vocational High School',
+    division: 'Football',
+    impact: ["Football Development"]
+};
--- index.ts
+++ index.ts
@@ -11,3 +11,30 @@ type Contract = {
 }
 
 type InternWithContract = Internship & Contract;
+
+const ihsan: InternWithContract = {
+    name: "Ihsan",
+    education: 'Vocational High School',
+    contractYear: 1
+}
+
+// b. Intersection with Interface
+interface Employee {
+    name: string,
+    education: string,
+    division: string
+}
+
+interface Volunteer {
+    name: string,
+    impact: string[]
+}
+
+interface volunteeringEmployee extends Employee, Volunteer {}
+
+const hafizh: volunteeringEmployee = {
+    name: 'Hafizh',
+    education: 'Vocational High School',
+    division: 'Football',
+    impact: ["Football Development"]
+}
