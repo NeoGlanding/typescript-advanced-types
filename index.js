8b0608f0b260cf9b153bf1d2262bd032953db5e1
NeoGlanding <neoglanding@protonmail.com>
2021-11-27 19:05:20 +0700

    type guards

--- index.js
+++ index.js
@@ -10,3 +10,70 @@ var hafizh = {
     division: 'Football',
     impact: ["Football Development"]
 };
+function addition(param1, param2) {
+    // Typeguard in If statement
+    if (typeof param1 === 'number' || typeof param2 === 'number') {
+        console.log(+param1 + +param2);
+    }
+    if (typeof param1 === 'string' || typeof param2 === 'string') {
+        console.log("".concat(param1.toString(), " ").concat(param2.toString()));
+    }
+    if ((typeof param1 === 'number' && typeof param2 === 'string') || (typeof param1 === 'string' && typeof param2 === 'number')) {
+        console.log('Enter value with same data types');
+    }
+}
+addition(1, 2);
+addition('Muhammad', 'Ihsan');
+addition(1, '3');
+// Typeguard in Object
+function employeeInfo(obj) {
+    console.log(obj.name);
+    if ('education' in obj && 'division' in obj) {
+        console.log('Education: ' + obj.education);
+        console.log('Division: ' + obj.division);
+    }
+    if ('impact' in obj) {
+        console.log('Impact: ' + obj.impact);
+    }
+}
+var eko = {
+    name: 'Eko Kurniawan Khannedy',
+    impact: ['Leading an Organization']
+};
+var galih = {
+    name: 'Sandhika Galih',
+    education: 'Postgraduate',
+    division: 'Software Engineer'
+};
+employeeInfo(eko);
+employeeInfo(galih);
+// Typeguards in Class
+var Teacher = /** @class */ (function () {
+    function Teacher() {
+    }
+    Teacher.prototype.info = function () {
+        console.log('She/He is a Teacher');
+    };
+    return Teacher;
+}());
+var UniversityTeacher = /** @class */ (function () {
+    function UniversityTeacher() {
+    }
+    UniversityTeacher.prototype.info = function () {
+        console.log('She/He is a University Teacher');
+    };
+    UniversityTeacher.prototype.where = function () {
+        console.log('She/he is teaching at some university');
+    };
+    return UniversityTeacher;
+}());
+function isYouTeacher(param) {
+    param.info();
+    if (param instanceof UniversityTeacher) {
+        param.where();
+    }
+}
+var onno = new Teacher();
+var danu = new UniversityTeacher();
+isYouTeacher(onno);
+isYouTeacher(danu);
--- index.ts
+++ index.ts
@@ -38,3 +38,82 @@ const hafizh: volunteeringEmployee = {
     division: 'Football',
     impact: ["Football Development"]
 }
+
+// 2. Type Guards
+type numberOrString = number | string;
+type unknownEmployee = Volunteer | Employee
+
+function addition(param1: numberOrString, param2: numberOrString): void {
+    // Typeguard in If statement
+    if (typeof param1 === 'number' || typeof param2 === 'number') {
+        console.log(+param1 + +param2)
+    }
+    if (typeof param1 === 'string' || typeof param2 === 'string') {
+        console.log(`${param1.toString()} ${param2.toString()}`)
+    }
+    if ((typeof param1 === 'number' && typeof param2 === 'string') || (typeof param1 === 'string' && typeof param2 === 'number')) {
+        console.log('Enter value with same data types')
+    }
+}
+
+addition(1,2);
+addition('Muhammad', 'Ihsan');
+addition(1,'3');
+
+// Typeguard in Object
+function employeeInfo(obj: unknownEmployee) {
+    console.log(obj.name);
+    if ('education' in obj && 'division' in obj) {
+        console.log('Education: ' + obj.education);
+        console.log('Division: ' + obj.division)
+    }
+    if ('impact' in obj) {
+        console.log('Impact: ' + obj.impact)
+    }
+}
+
+const eko: unknownEmployee = {
+    name: 'Eko Kurniawan Khannedy',
+    impact: ['Leading an Organization']
+}
+
+const galih: unknownEmployee = {
+    name: 'Sandhika Galih',
+    education: 'Postgraduate',
+    division: 'Software Engineer'
+}
+
+employeeInfo(eko);
+employeeInfo(galih)
+
+// Typeguards in Class
+class Teacher {
+    info() {
+        console.log('She/He is a Teacher')
+    }
+}
+
+class UniversityTeacher {
+    info() {
+        console.log('She/He is a University Teacher')
+    }
+
+    where() {
+        console.log('She/he is teaching at some university')
+    }
+}
+
+type unknownTeacher = Teacher | UniversityTeacher
+
+function isYouTeacher(param: unknownTeacher) {
+    param.info()
+    if (param instanceof UniversityTeacher) {
+        param.where()
+    }
+}
+
+const onno = new Teacher();
+const danu = new UniversityTeacher();
+
+isYouTeacher(onno);
+isYouTeacher(danu);
